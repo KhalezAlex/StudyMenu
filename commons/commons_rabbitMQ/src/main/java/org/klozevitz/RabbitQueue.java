dc52d56049dd2d0531f65fd651a81e83a313283a
khalezov.aleksei <khalezov.aleksei@otr.ru>
2025-01-16 23:41:09 +0300

    модуль commons_JPA- добавлены очереди для всех трех ботов (возможно, лучше разделить - надо подумать)

---
+++ commons/commons_rabbitMQ/src/main/java/org/klozevitz/RabbitQueue.java
@@ -0,0 +1,23 @@
+package org.klozevitz;
+
+/**
+ * Подумать над тем, кто будет грузить доукменты - кажется, что это должны делать отделения, а не компания
+ * */
+
+public class RabbitQueue {
+    public static final String COMPANY_TEXT_UPDATE = "company_text_update";
+    public static final String COMPANY_COMMAND_UPDATE = "company_command_update";
+    public static final String COMPANY_CALLBACK_QUERY_UPDATE = "company_callback_query_update";
+    public static final String COMPANY_ANSWER_MESSAGE = "company_answer_message";
+
+    public static final String DEPARTMENT_TEXT_UPDATE = "department_text_update";
+    public static final String DEPARTMENT_COMMAND_UPDATE = "department_command_update";
+    public static final String DEPARTMENT_CALLBACK_QUERY_UPDATE = "department_callback_query_update";
+    public static final String DEPARTMENT_DOC_UPDATE = "department_doc_update";
+    public static final String DEPARTMENT_ANSWER_MESSAGE = "department_answer_message";
+
+    public static final String EMPLOYEE_TEXT_UPDATE = "employee_text_update";
+    public static final String EMPLOYEE_COMMAND_UPDATE = "employee_command_update";
+    public static final String EMPLOYEE_CALLBACK_QUERY_UPDATE = "employee_callback_query_update";
+    public static final String EMPLOYEE_ANSWER_MESSAGE = "employee_answer_message";
+}
