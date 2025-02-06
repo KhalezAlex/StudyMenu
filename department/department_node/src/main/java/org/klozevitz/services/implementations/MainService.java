919c689a6382ae31eccba00391c2dfc0f737d174
khalezov.aleksei <khalezov.aleksei@otr.ru>
2025-02-06 20:53:31 +0300

    - переписан метод MainService.findAppUser- новая логика из-за того, что пользователи сюда попадают сверху

--- department/department_node/src/main/java/org/klozevitz/services/implementations/MainService.java
+++ department/department_node/src/main/java/org/klozevitz/services/implementations/MainService.java
@@ -1,13 +1,23 @@
 package org.klozevitz.services.implementations;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import org.klozevitz.enitites.appUsers.AppUser;
+import org.klozevitz.repositories.appUsers.AppUserRepo;
 import org.klozevitz.services.interfaces.Main;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+
+import java.util.Optional;
 
 @Log4j
 @Service
+@RequiredArgsConstructor
 public class MainService  implements Main {
+    private final AppUserRepo appUserRepo;
+
+
     @Override
     public void processTextMessage(Update update) {
 
@@ -27,4 +37,32 @@ public class MainService  implements Main {
     public void processDocMessage(Update update) {
 
     }
+
+    private long chatId(Update update) {
+        return update.hasMessage() ?
+                update.getMessage().getChatId() :
+                update.getCallbackQuery().getMessage().getChatId();
+    }
+
+    private Optional<AppUser> findAppUser(Update update) {
+        var telegramUser = telegramUserFromUpdate(update);
+        var persistentApplicationUser = appUserRepo.findByTelegramUserId(telegramUser.getId());
+
+        if (persistentApplicationUser.isEmpty()) {
+            return Optional.empty();
+        }
+
+        if (persistentApplicationUser.get().getDepartment() == null) {
+            return Optional.empty();
+        }
+
+        return persistentApplicationUser;
+    }
+
+    private User telegramUserFromUpdate(Update update) {
+        return update.hasMessage() ?
+                update.getMessage().getFrom() :
+                update.getCallbackQuery().getFrom();
+    }
+
 }
