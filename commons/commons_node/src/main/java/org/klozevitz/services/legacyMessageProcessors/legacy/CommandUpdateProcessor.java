fa6a7c819aea8bacd075ce55312703f53a1f5ea0
khalezov.aleksei <khalezov.aleksei@otr.ru>
2025-02-15 22:56:05 +0300

    удалено старое дерево интерфейсов сервисов

--- commons/commons_node/src/main/java/org/klozevitz/services/legacyMessageProcessors/legacy/CommandUpdateProcessor.java
+++
@@ -1,9 +0,0 @@
-package org.klozevitz.services.legacyMessageProcessors.legacy;
-
-import org.klozevitz.enitites.appUsers.AppUser;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Update;
-
-public interface CommandUpdateProcessor {
-    SendMessage processCommandUpdate(Update update, AppUser currentAppUser);
-}
