b8639a24e771135b23dacf7e09f5e6eecc9e6f90
khalezov.aleksei <khalezov.aleksei@otr.ru>
2025-02-19 20:42:42 +0300

    - релизованы базовые update-обработчики

---
+++ employee/employee_node/src/main/java/org/klozevitz/services/implementations/updateProcessors/callbackQueryUpdateProcessors/CallbackQueryEmployeeUP.java
@@ -0,0 +1,18 @@
+package org.klozevitz.services.implementations.updateProcessors.callbackQueryUpdateProcessors;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.klozevitz.enitites.appUsers.AppUser;
+import org.klozevitz.services.messageProcessors.UpdateProcessor;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Log4j
+@RequiredArgsConstructor
+public class CallbackQueryEmployeeUP implements UpdateProcessor {
+
+    @Override
+    public SendMessage processUpdate(Update update, AppUser currentAppUser) {
+        return null;
+    }
+}
--- employee/employee_node/src/main/java/org/klozevitz/services/implementations/updateProcessors/util/NotRegisteredAppUserEmployeeUP.java
+++ employee/employee_node/src/main/java/org/klozevitz/services/implementations/updateProcessors/util/NotRegisteredAppUserEmployeeUP.java
@@ -1,20 +1,18 @@
 package org.klozevitz.services.implementations.updateProcessors.util;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j;
-import org.klozevitz.DepartmentTelegramView;
 import org.klozevitz.services.messageProcessors.WrongAppUserDataUpdateProcessor;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Log4j
 @RequiredArgsConstructor
-public class NotRegisteredAppUserDepartmentUP implements WrongAppUserDataUpdateProcessor {
-    private final DepartmentTelegramView telegramView;
-
+public class NotRegisteredAppUserEmployeeUP implements WrongAppUserDataUpdateProcessor {
     @Override
     public SendMessage processUpdate(Update update) {
-        return telegramView.notRegisteredDepartmentErrorView(update);
+        return null;
     }
+    //    private final EmployeeTelegramView telegramView;
+
 }
